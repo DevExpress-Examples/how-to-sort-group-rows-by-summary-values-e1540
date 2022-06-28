ebd4cf66ac49f8df853dcd0c124b7dc2531ba2b1
DevExpressExampleBot <fakeEmail>
2022-06-28 16:19:19 +0400

    Source auto update [skip ci]

--- VB/DXSample_SortGroupsBySummary/Window1.xaml.vb
+++ VB/DXSample_SortGroupsBySummary/Window1.xaml.vb
@@ -1,64 +1,44 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Windows
-Imports System.Windows.Documents
 Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Collections.ObjectModel
+Imports System.Windows
 
 Namespace DXSample_SortGroupsBySummary
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = New AccountList().GetData()
-			SortGroupsBySummary(view.GroupedColumns(0))
-		End Sub
-		Private Sub SortGroupsBySummary(ByVal column As GridColumn)
-			Dim sortInfo As New GridGroupSummarySortInfo(grid.GroupSummary(0), column.FieldName, System.ComponentModel.ListSortDirection.Ascending)
-			grid.GroupSummarySortInfo.Add(sortInfo)
-		End Sub
-	End Class
-	Public Class AccountList
-		Public Function GetData() As List(Of Account)
-			Return CreateAccounts()
-		End Function
-		Private Function CreateAccounts() As List(Of Account)
-			Dim list As New List(Of Account)()
-			list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = DateTime.Today, .Age = 57})
-			list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New DateTime(2009, 5, 10), .Age = 24})
-			list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New DateTime(2009, 5, 10), .Age = 35})
-			list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = DateTime.Today, .Age = 19})
-			list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New DateTime(2008, 10, 18), .Age = 42})
-			Return list
-		End Function
-	End Class
-	Public Class Account
-		Private privateUserName As String
-		Public Property UserName() As String
-			Get
-				Return privateUserName
-			End Get
-			Set(ByVal value As String)
-				privateUserName = value
-			End Set
-		End Property
-		Private privateRegistrationDate As DateTime
-		Public Property RegistrationDate() As DateTime
-			Get
-				Return privateRegistrationDate
-			End Get
-			Set(ByVal value As DateTime)
-				privateRegistrationDate = value
-			End Set
-		End Property
-		Private privateAge As Integer
-		Public Property Age() As Integer
-			Get
-				Return privateAge
-			End Get
-			Set(ByVal value As Integer)
-				privateAge = value
-			End Set
-		End Property
-	End Class
+
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = AccountList.GetData()
+            Me.SortGroupsBySummary(Me.view.GroupedColumns(0))
+        End Sub
+
+        Private Sub SortGroupsBySummary(ByVal column As GridColumn)
+            Dim sortInfo As GridGroupSummarySortInfo = New GridGroupSummarySortInfo(Me.grid.GroupSummary(0), column.FieldName, System.ComponentModel.ListSortDirection.Ascending)
+            Me.grid.GroupSummarySortInfo.Add(sortInfo)
+        End Sub
+    End Class
+
+    Public Class AccountList
+
+        Public Shared Function GetData() As ObservableCollection(Of Account)
+            Dim list As ObservableCollection(Of Account) = New ObservableCollection(Of Account)()
+            list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = Date.Today, .Age = 57})
+            list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New DateTime(2009, 5, 10), .Age = 24})
+            list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New DateTime(2009, 5, 10), .Age = 35})
+            list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = Date.Today, .Age = 19})
+            list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New DateTime(2008, 10, 18), .Age = 42})
+            Return list
+        End Function
+    End Class
+
+    Public Class Account
+
+        Public Property UserName As String
+
+        Public Property RegistrationDate As Date
+
+        Public Property Age As Integer
+    End Class
 End Namespace
